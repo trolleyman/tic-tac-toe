92168cd2aeb9ee2bc1c123bec66c614120620791
trolleyman <cgtrolley@gmail.com>
2016-02-15 12:58:04 +0000

    Added gtk::init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 extern crate gtk;
 
+use std::process::exit;
+
 fn main() {
-	
+	match gtk::init() {
+		Ok(_) => {
+			println!("GTK initialized.");
+		},
+		Err(_) => {
+			println!("Error: GTK could not be initialized.");
+			exit(1);
+		},
+	}
 }
